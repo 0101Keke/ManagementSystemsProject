3ac119e838084f1f9cd021a65152dd46cc413926
0101Keke <kekeletsomaleka04@gmail.com>
2024-11-07 08:28:19 +0200

    Add project files.

---
+++ DataLayer/FileHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+
+namespace ManagementSystemsProject.DataLayer
+{
+    internal class FileHandler
+    {
+       
+        public FileHandler() 
+        {}
+
+        public void AddStudent(Student student, string Path) 
+        {
+            FileStream fs = new FileStream(Path, FileMode.OpenOrCreate);
+            StreamWriter sw = new StreamWriter(fs);
+
+
+            sw.WriteLine(student.StudentID + "," + student.Name + "," + student.Age + "," + student.Course);
+
+            sw.Close();
+            fs.Close();
+
+
+            try
+            {
+                MessageBox.Show("Added Successfully");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ManagementSystemsProject
+{
+    public partial class frmMain : Form
+    {
+
+        
+        public frmMain()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pnlProject_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pnlCore_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            frmAdd add = new frmAdd();
+            add.Show();
+        }
+    }
+}
