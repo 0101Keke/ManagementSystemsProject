4cde6102f6d7eaa3aa38f4c7975b786f63729e89
0101Keke <kekeletsomaleka04@gmail.com>
2024-11-08 00:21:54 +0200

    added exit button to view form

--- ViewForm.Designer.cs
+++ ViewForm.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.dataGridView3 = new System.Windows.Forms.DataGridView();
             this.btnBack2 = new System.Windows.Forms.Button();
+            this.btnExit1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
             this.SuspendLayout();
             // 
@@ -47,17 +48,27 @@
             // 
             this.btnBack2.Location = new System.Drawing.Point(12, 416);
             this.btnBack2.Name = "btnBack2";
-            this.btnBack2.Size = new System.Drawing.Size(121, 23);
+            this.btnBack2.Size = new System.Drawing.Size(121, 38);
             this.btnBack2.TabIndex = 3;
             this.btnBack2.Text = "Back";
             this.btnBack2.UseVisualStyleBackColor = true;
             this.btnBack2.Click += new System.EventHandler(this.btnBack2_Click);
             // 
+            // btnExit1
+            // 
+            this.btnExit1.Location = new System.Drawing.Point(681, 416);
+            this.btnExit1.Name = "btnExit1";
+            this.btnExit1.Size = new System.Drawing.Size(125, 38);
+            this.btnExit1.TabIndex = 4;
+            this.btnExit1.Text = "Exit";
+            this.btnExit1.UseVisualStyleBackColor = true;
+            // 
             // ViewForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(818, 451);
+            this.ClientSize = new System.Drawing.Size(818, 466);
+            this.Controls.Add(this.btnExit1);
             this.Controls.Add(this.btnBack2);
             this.Controls.Add(this.dataGridView3);
             this.Name = "ViewForm";
@@ -71,5 +82,6 @@
 
         private System.Windows.Forms.DataGridView dataGridView3;
         private System.Windows.Forms.Button btnBack2;
+        private System.Windows.Forms.Button btnExit1;
     }
 }
