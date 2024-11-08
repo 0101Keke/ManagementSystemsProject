119a3abcf291c54f68107d33bdcd36415bb53f93
Tumiso Lethabo Koee <600441@student.belgiumcampus.ac.za>
2024-11-08 22:39:55 +0200

    This is the reoprt summary

---
+++ DataLayer/ReportSummary.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ManagementSystemsProject.DataLayer
+{
+    public class ReportSummary
+    {
+        private readonly string filepath;
+
+        public ReportSummary(string filepath)
+        {
+            this.filepath = filepath;
+        }
+
+        public int TotalStudents()
+        {
+            try
+            {
+                if (!File.Exists(filepath))
+                {
+                    throw new FileNotFoundException("The student file was not found.");
+                }
+
+                var lines = File.ReadAllLines(filepath);
+                return lines.Length;
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return 0;
+            }
+            catch(Exception ex) 
+            { 
+                return 0; 
+            }
+        }
+
+        public double AvgAge()
+        {
+            try
+            {
+                if (!File.Exists(filepath))
+                {
+                    throw new FileNotFoundException("The student file was not found.");
+                }
+
+                string ages = File.ReadAllLines(filepath).Select(line =>
+                {
+                    var parts = line.Split(',');
+                    if (parts.Length < 3 || !int.TryParse(parts[2], out int age))
+                    {
+                        throw new FormatException("Invalid data format for age.")
+                    }
+                    return age;
+                }).ToList();
+
+                return ages.Any() ? ages.Average() : 0;
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return 0;
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("An error occured while calculating the average age: " + ex.Message)
+                return 0; 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occured while calculating the average age: " + ex.Message);
+                return 0;
+            }
+        }
+    }
+
+}
--- Forms/GenerateForm.Designer.cs
+++ Forms/GenerateForm.Designer.cs
@@ -28,21 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.dgvTotal = new System.Windows.Forms.DataGridView();
             this.btnBack2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.dgvAverage = new System.Windows.Forms.DataGridView();
+            this.btnTotal = new System.Windows.Forms.Button();
+            this.btnAvgAge = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTotal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAverage)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView3
+            // dgvTotal
             // 
-            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView3.Location = new System.Drawing.Point(15, 12);
-            this.dataGridView3.Name = "dataGridView3";
-            this.dataGridView3.RowHeadersWidth = 51;
-            this.dataGridView3.RowTemplate.Height = 24;
-            this.dataGridView3.Size = new System.Drawing.Size(794, 396);
-            this.dataGridView3.TabIndex = 3;
+            this.dgvTotal.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTotal.Location = new System.Drawing.Point(15, 12);
+            this.dgvTotal.Name = "dgvTotal";
+            this.dgvTotal.RowHeadersWidth = 51;
+            this.dgvTotal.RowTemplate.Height = 24;
+            this.dgvTotal.Size = new System.Drawing.Size(376, 322);
+            this.dgvTotal.TabIndex = 3;
             // 
             // btnBack2
             // 
@@ -62,25 +66,61 @@
             this.button1.Text = "Exit";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // dgvAverage
+            // 
+            this.dgvAverage.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAverage.Location = new System.Drawing.Point(433, 12);
+            this.dgvAverage.Name = "dgvAverage";
+            this.dgvAverage.RowHeadersWidth = 51;
+            this.dgvAverage.RowTemplate.Height = 24;
+            this.dgvAverage.Size = new System.Drawing.Size(376, 322);
+            this.dgvAverage.TabIndex = 6;
+            // 
+            // btnTotal
+            // 
+            this.btnTotal.Location = new System.Drawing.Point(154, 364);
+            this.btnTotal.Name = "btnTotal";
+            this.btnTotal.Size = new System.Drawing.Size(109, 38);
+            this.btnTotal.TabIndex = 7;
+            this.btnTotal.Text = "Total Student";
+            this.btnTotal.UseVisualStyleBackColor = true;
+            this.btnTotal.Click += new System.EventHandler(this.btnTotal_Click);
+            // 
+            // btnAvgAge
+            // 
+            this.btnAvgAge.Location = new System.Drawing.Point(565, 364);
+            this.btnAvgAge.Name = "btnAvgAge";
+            this.btnAvgAge.Size = new System.Drawing.Size(109, 38);
+            this.btnAvgAge.TabIndex = 8;
+            this.btnAvgAge.Text = "Average Age";
+            this.btnAvgAge.UseVisualStyleBackColor = true;
+            // 
             // GenerateForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(821, 476);
+            this.Controls.Add(this.btnAvgAge);
+            this.Controls.Add(this.btnTotal);
+            this.Controls.Add(this.dgvAverage);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnBack2);
-            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dgvTotal);
             this.Name = "GenerateForm";
             this.Text = "GenerateForm";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTotal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAverage)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridView dgvTotal;
         private System.Windows.Forms.Button btnBack2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dgvAverage;
+        private System.Windows.Forms.Button btnTotal;
+        private System.Windows.Forms.Button btnAvgAge;
     }
 }
