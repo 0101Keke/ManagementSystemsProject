fc5a3f9464036e313df9d72540c88696d9325425
BKGitRepo <user1@DESKTOP-AARLF9A>
2024-11-10 08:30:39 +0200

    implemented th update button funtionality

--- DataLayer/FileHandler.cs
+++ DataLayer/FileHandler.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Windows.Forms;
+using System.Security.Cryptography.X509Certificates;
 
 
 namespace ManagementSystemsProject.DataLayer
@@ -14,6 +15,8 @@ namespace ManagementSystemsProject.DataLayer
        
         public FileHandler() 
         {}
+
+        public static readonly string filePath = "C:\\Users\\user1\\source\\repos\\ManagementSystemsProject\\DataLayer\\students.txt";
 
         public void AddStudent(Student student, string Path) 
         {
@@ -34,7 +37,53 @@ namespace ManagementSystemsProject.DataLayer
             catch (Exception e)
             {
                 MessageBox.Show(e.Message);
+            }  
+        }
+
+        public static List<Student> GetAllStudents()
+        {
+            var students = new List<Student>();
+
+            if (File.Exists(filePath))
+            {
+                var lines = File.ReadAllLines(filePath);
+                foreach (var line in lines)
+                {
+                    var parts = line.Split(',');
+
+                    if (parts.Length == 4)
+                    {
+                        var student = new Student(int.Parse(parts[0]), parts[1], int.Parse(parts[2]), parts[3]);
+                        students.Add(student);
+                    }
+                }
             }
+            else
+            {
+                throw new FileNotFoundException("The students.txt file was not found.");
+            }
+
+            return students;
+        }
+
+        public static void UpdateStudent(Student updatedStudent)
+        {
+            if (!File.Exists(filePath)) throw new FileNotFoundException("The students.txt file was not found.");
+
+            var lines = File.ReadAllLines(filePath);
+            for (int i = 0; i < lines.Length; i++)
+            {
+                var parts = lines[i].Split(',');
+                if (int.Parse(parts[0]) == updatedStudent.StudentID)
+                {
+                    // Update line with new student details
+                    lines[i] = $"{updatedStudent.StudentID},{updatedStudent.Name},{updatedStudent.Age},{updatedStudent.Course}";
+                    break;
+                }
+            }
+
+            // Write updated data back to the file
+            File.WriteAllLines(filePath, lines);
         }
     }
 }
